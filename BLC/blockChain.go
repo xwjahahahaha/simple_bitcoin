3b27e155b956b7f51d13c975dd1ae746efddb75f
xwjahahahaha <1438575858@qq.com>
2021-02-23 16:51:56 +0800

    区块链添加区块

--- BLC/blockChain.go
+++ BLC/blockChain.go
@@ -17,3 +17,15 @@ func CreateNewBC() *BlockChain {
 	}
 }
 
+//区块链中添加新的区块
+func (bc *BlockChain) AddNewBlock(data string)  {
+	length := len(bc.Blocks)
+	lastBlock := bc.Blocks[length-1]
+	//计算新区块数据
+	newHeight, preHash := int64(length), lastBlock.Hash
+	//生成新区块
+	newBlock := NewBlock(data, newHeight, preHash)
+	//加入区块链
+	bc.Blocks = append(bc.Blocks, newBlock)
+}
+
--- main.go
+++ main.go
@@ -7,8 +7,21 @@ import (
 
 func main() {
 
-	newBlockChain := BLC.CreateNewBC()
-	fmt.Println(newBlockChain)
-	fmt.Println(newBlockChain.Blocks)
-	fmt.Println(newBlockChain.Blocks[0])
+	blockChain := BLC.CreateNewBC()
+	//fmt.Println(newBlockChain)
+	//fmt.Println(newBlockChain.Blocks)
+	//fmt.Println(newBlockChain.Blocks[0])
+
+
+	//添加新区块
+	blockChain.AddNewBlock("a send $1 to b")
+	blockChain.AddNewBlock("a send $2 to b")
+	blockChain.AddNewBlock("a send $3 to b")
+	fmt.Println(blockChain)
+	fmt.Println(blockChain.Blocks)
+	fmt.Println(blockChain.Blocks[2])
+
+
+
+
 }
